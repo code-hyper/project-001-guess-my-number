46a103274e4ab2ccc2656c51be1785668d628adc
code-hyper <geminiatwork@gmail.com>
2022-06-07 12:40:54 +0800

    Coding Challenge #1

--- script.js
+++ script.js
@@ -10,8 +10,7 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
  */
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
 document.querySelector('.check').addEventListener('click', function () {
@@ -25,6 +24,8 @@ document.querySelector('.check').addEventListener('click', function () {
     // When players win
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+    document.querySelector('.number').textContent = secretNumber;
 
     document.querySelector('body').style.backgroundColor = '#60b347';
 
@@ -52,4 +53,29 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+});
+
+/* Coding Challenge #1
+Implement a game rest functionality, so that the player can make a new guess!
+
+Your tasks:
+1. Select the element with the 'again' class and attach a click event handler
+2. In the handler function, restore initial values of the 'score' and
+'secretNumber' variables
+3. Restore the initial conditions of the message, number, score and guess input
+fields
+4. Also restore the original background color (#222) and number width (15rem)
+
+GOOD LUCK 😀 */
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
 });
